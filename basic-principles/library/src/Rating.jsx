f83da0401c01a91f71338ffa3acdf5588117d35e
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-12-13 09:08:44 -0500

    updating solution

--- basic-principles/library/src/Rating.jsx
+++ basic-principles/library/src/Rating.jsx
@@ -2,12 +2,12 @@ import PropTypes from "prop-types";
 import {StarBorder, Star} from '@mui/icons-material';
 import './Rating.css';
 
-function Rating({item, onRate}) {
+function Rating({item}) {
     const ratings = [];
     for (let i = 1; i <= 5; i++) {
         ratings.push(
-            <button onClick={() => onRate(item.id, i)}
-                    className="ratingButton"
+            <button className="ratingButton"
+                    data-value={i}
                     key={i}>
                 {item.rating < i ? <StarBorder/> : <Star/>}
             </button>
@@ -21,8 +21,7 @@ Rating.propTypes = {
             id: PropTypes.number.isRequired,
             rating: PropTypes.number.isRequired
         }
-    ).isRequired,
-    onRateChange: PropTypes.func.isRequired,
+    ).isRequired
 };
 
 export default Rating;
