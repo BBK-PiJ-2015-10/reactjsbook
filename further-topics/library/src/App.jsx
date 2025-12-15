c113ebca61c8aaed263d1f3739e1b45ba43546c4
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-12-15 09:46:49 -0500

    using render props

--- further-topics/library/src/App.jsx
+++ further-topics/library/src/App.jsx
@@ -3,21 +3,28 @@ import './App.css';
 //import BooksList from "./BooksList.container";
 import withBooks from "./withBooks";
 import BooksList from "./BooksList";
+import BooksLoader from "./BooksLoader";
 
 const BooksListWithBooks = withBooks(BooksList);
 
 function App() {
-  return (
-      <div>
-          <h1>Books Management</h1>
-          <BooksListWithBooks />
-      </div>
-  );
-   // <div>
-   //     <h1>Books Management</h1>
-   //       <BooksList />
-   // </div>
-   //    );
+    return (
+        <div>
+            <h1>Books management</h1>
+            <BooksLoader>
+                {(b, e) => <BooksList books={b} error={e}/>}
+            </BooksLoader>
+        </div>
+        // <div>
+        //     <h1>Books Management</h1>
+        //     <BooksListWithBooks />
+        // </div>
+    );
+    // <div>
+    //     <h1>Books Management</h1>
+    //       <BooksList />
+    // </div>
+    //    );
 }
 
 export default App;
