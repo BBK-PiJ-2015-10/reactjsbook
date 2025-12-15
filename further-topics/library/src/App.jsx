8cf2416721bbc0725cc4027f8a9d25329468c2cc
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-12-15 07:44:43 -0500

    HOF example p1

--- further-topics/library/src/App.jsx
+++ further-topics/library/src/App.jsx
@@ -1,14 +1,23 @@
 import './App.css';
 
-import BooksList from "./BooksList.container";
+//import BooksList from "./BooksList.container";
+import withBooks from "./withBooks";
+import BooksList from "./BooksList";
+
+const BooksListWithBooks = withBooks(BooksList);
 
 function App() {
   return (
-   <div>
-       <h1>Books Management</h1>
-         <BooksList />
-   </div>
-      );
+      <div>
+          <h1>Books Management</h1>
+          <BooksListWithBooks />
+      </div>
+  );
+   // <div>
+   //     <h1>Books Management</h1>
+   //       <BooksList />
+   // </div>
+   //    );
 }
 
 export default App;
