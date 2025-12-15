f0163ef663d92dd1529abcd8b90a5bae8575ed7f
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-12-15 10:22:53 -0500

    Context example

--- further-topics/library/src/App.jsx
+++ further-topics/library/src/App.jsx
@@ -1,25 +1,43 @@
 import './App.css';
 
 //import BooksList from "./BooksList.container";
-import withBooks from "./withBooks";
-import BooksList from "./BooksList";
-import BooksLoader from "./BooksLoader";
+//import withBooks from "./withBooks";
+//import BooksList from "./BooksList";
+//import BooksLoader from "./BooksLoader";
+import {useState} from "react";
+import Context from "./Context";
+import Counter from "./Counter";
 
-const BooksListWithBooks = withBooks(BooksList);
+//const BooksListWithBooks = withBooks(BooksList);
 
 function App() {
+
+    const [counter, setCounter] = useState(0);
+
+    function increment() {
+        setCounter((prevState) => prevState + 1);
+    }
+
     return (
-        <div>
-            <h1>Books management</h1>
-            <BooksLoader>
-                {(b, e) => <BooksList books={b} error={e}/>}
-            </BooksLoader>
-        </div>
-        // <div>
-        //     <h1>Books Management</h1>
-        //     <BooksListWithBooks />
-        // </div>
+        <Context.Provider value={counter}>
+            <Counter/>
+            <button onClick={increment}>increment</button>
+        </Context.Provider>
     );
+
+
+    //return (
+    // <div>
+    //     <h1>Books management</h1>
+    //     <BooksLoader>
+    //         {(b, e) => <BooksList books={b} error={e}/>}
+    //     </BooksLoader>
+    // </div>
+    // <div>
+    //     <h1>Books Management</h1>
+    //     <BooksListWithBooks />
+    // </div>
+    //);
     // <div>
     //     <h1>Books Management</h1>
     //       <BooksList />
