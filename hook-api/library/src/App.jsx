36063a95c08eac10bcca913dcd66bf284c424370
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2025-12-18 14:51:48 -0500

    adding hooks api

---
+++ hook-api/library/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import BooksListUsingReducer from "./BooksListUsingReducer";
+
+function App() {
+  return (
+   <div>
+       <h1>Books Management</h1>
+        <BooksListUsingReducer />
+   </div>
+      );
+}
+
+export default App;
---
+++ hook-api/library/src/BooksListUsingReducer.jsx
@@ -0,0 +1,68 @@
+import {useReducer} from 'react';
+import './BooksList.css';
+
+const initialBooks = [
+    {
+        id: 1,
+        title: 'first book',
+        author: 'god',
+        isbn: '12343',
+        rating: 5
+    },
+    {
+        id: 2,
+        title: 'second book',
+        isbn: '4567',
+        rating: 4
+    },
+    {
+        id: 3,
+        title: 'third book',
+        author: 'godana',
+        isbn: '891011',
+        rating: 2
+    }
+];
+
+function BooksListUsingReducer() {
+
+
+
+    const [books, setBooks] = useState(initialBooks);
+
+    function handleRate(id, rating) {
+        setBooks((prevState) => {
+            return prevState.map((book) => {
+                if (book.id === id) {
+                    book.rating = rating;
+                }
+                return book;
+            });
+        });
+    }
+
+    if (books.length === 0) {
+        return <div>No books found</div>;
+    } else {
+        return (
+            <table>
+                <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>ISBN</th>
+                    <th>Rating</th>
+                </tr>
+                </thead>
+                <tbody>
+                {books.map((book) => (
+                    <BooksListItem key={book.id} book={book} onRate={handleRate}/>
+                ))}
+                </tbody>
+            </table>
+        );
+    }
+}
+
+export default BooksListUsingReducer;
