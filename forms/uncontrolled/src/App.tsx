d4ba80f8029611bf66324348c96903cffa9ba36b
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2026-01-26 08:12:40 -0500

    uncontrolled p1

---
+++ forms/uncontrolled/src/App.tsx
@@ -0,0 +1,42 @@
+import React, {useRef} from "react";
+import './App.css';
+
+const App: React.FC = () => {
+        const inputRef = useRef<HTMLInputElement>(null);
+
+        function handleClick() {
+            console.log(`Someone sent ${inputRef.current!.value}`)
+        }
+
+        return (
+            <div>
+                <input type="text" ref={inputRef}/>
+                <button onClick={handleClick}>ok</button>
+            </div>
+        )
+    }
+;
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
