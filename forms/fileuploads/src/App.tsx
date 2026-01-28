2020f66fd94b0dc38b5dc7607054511c3d205c38
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2026-01-28 15:14:28 -0500

    seting up backend app

---
+++ forms/fileuploads/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+       WOOF WOOF
+    </div>
+  );
+}
+
+export default App;
---
+++ forms/fileuploads/src/Book.tsx
@@ -0,0 +1,9 @@
+export type Book = {
+    id: number;
+    title: string;
+    image?: string;
+};
+
+export type InputBook = Omit<Book, 'id'> & {
+    id?: number;
+};
