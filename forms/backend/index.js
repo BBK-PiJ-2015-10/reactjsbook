cc3665b200aaff383cf44e881c8a94742f594dfa
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2026-01-29 09:52:59 -0500

    submitting file

--- forms/backend/index.js
+++ forms/backend/index.js
@@ -15,48 +15,14 @@ server.use(fileUpload());
 server.use(express.urlencoded({extended: true}));
 server.use(cors());
 
-// server.get('/health', (req, res) => {
-//     console.log('Something called health check');
-//     res.send("Health is ok");
-// })
-
-
-// server.put('/books', (req, resp, next) => {
-//     if (req.files.image) {
-//         const file = req.files.image;
-//         const name = req.body.title.replace(' ', '_');
-//         const image = `${name}.png`;
-//         file.mv(`${__dirname}/../public/${image}`);
-//         console.log("WAKE UP");
-//         resp.body.image;
-//     } else {
-//         console.log("CAT")
-//     }
-//     next();
-// });
-
-// server.post('/books', (req, resp, next) => {
-//     if (req.files.image) {
-//         const file = req.files.image;
-//         const name = req.body.title.replace(' ', '_');
-//         const image = `${name}.png`;
-//         file.mv(`${__dirname}/../public/${image}`);
-//         console.log("WAKE UP");
-//         resp.body.image;
-//     } else {
-//         console.log("CAT")
-//     }
-//     next();
-// });
-
 server.use('/books', (req, Response, next) => {
     if ((req.method === 'POST' | req.method === 'PUT') && req.files.image) {
         const file = req.files.image;
         const name = req.body.title.replace(' ', '_');
         const image = `${name}.png`;
-        file.mv(`${__dirname}/../public/${image}`);
+        file.mv(`${__dirname}/public/${image}`);
         console.log("WAKE UP");
-        resp.body.image = image
+        req.body.image = image
     } else {
         console.log("got something else");
         //req.body.image = '';
--- forms/fileuploads/src/App.tsx
+++ forms/fileuploads/src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import axios from "axios";
+import {useEffect, useState} from "react";
+import React from "react";
+import {Book} from "./Book";
+import Form from './Form';
+
+const App: React.FC = () => {
+
+    const [books, setBooks] = useState<Book[]>([]);
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    async function fetchData() {
+        const {data} = await axios.get<Book[]>('http://localhost:3001/books');
+        setBooks(data);
+    }
+
+    async function handleSubmit(formdata: FormData) {
+        await axios.post('http://localhost:3001/books', formdata, {
+            headers: {
+                'content-type': 'multipart/form-data',
+            },
+        });
+        fetchData();
+    }
+
+    return (
+        <div>
+            <Form onSave={handleSubmit}/>
+            <hr/>
+            <ul>
+                {books.map((book) => (
+                    <li key={book.id}>
+                        {book.title}
+                        {book.image && (<img src={book.image} height="40"
+                                             width="40" alt={book.title}/>
+                        )}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
