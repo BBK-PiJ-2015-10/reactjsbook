799a0da727b2cf2b988f5be8e472624492e6879c
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2026-02-04 08:35:03 -0500

    adding filters

--- libraries/src/List.tsx
+++ libraries/src/List.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import {
-    Paper, Table, TableBody, TableCell, TableHead, TableRow
+    Paper, Table, TableBody, TableCell, TableHead, TableRow, TextField
 } from "@mui/material";
 import {StarBorder, Star} from '@mui/icons-material';
 import {Book} from './Book'
@@ -10,8 +10,15 @@ type Props = {
 }
 
 const List: React.FC<Props> = ({books}) => {
+
+    const [filter, setFilter] = useState('');
+
     return (
         <Paper>
+            <TextField label="Filter list"
+                       value={filter}
+                       onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                           setFilter(event.target.value)}/>
             <Table>
                 <TableHead>
                     <TableRow>
@@ -22,23 +29,26 @@ const List: React.FC<Props> = ({books}) => {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {books.map((book) => (
-                        <TableRow key={book.id}>
-                            <TableCell>{book.title}</TableCell>
-                            <TableCell>{book.author}</TableCell>
-                            <TableCell>{book.isbn}</TableCell>
-                            <TableCell>
-                                {Array(5)
-                                    .fill('')
-                                    .map((rating, index) =>
-                                        book.rating <= index ? (
-                                            <StarBorder key={index}/>
-                                        ) : (<Star key={index}/>
-                                        )
-                                    )}
-                            </TableCell>
-                        </TableRow>
-                    ))}
+                    {books
+                        .filter((book) =>
+                            book.title.toLowerCase().includes(filter.toLowerCase()))
+                        .map((book) => (
+                            <TableRow key={book.id}>
+                                <TableCell>{book.title}</TableCell>
+                                <TableCell>{book.author}</TableCell>
+                                <TableCell>{book.isbn}</TableCell>
+                                <TableCell>
+                                    {Array(5)
+                                        .fill('')
+                                        .map((rating, index) =>
+                                            book.rating <= index ? (
+                                                <StarBorder key={index}/>
+                                            ) : (<Star key={index}/>
+                                            )
+                                        )}
+                                </TableCell>
+                            </TableRow>
+                        ))}
                 </TableBody>
             </Table>
         </Paper>
