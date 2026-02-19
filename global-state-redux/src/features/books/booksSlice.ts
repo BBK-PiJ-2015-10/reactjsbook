4e26550ec28f8f9f418d455d9dd749e3c2f6bdcf
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2026-02-18 19:05:06 -0500

    updating solution

--- global-state-redux/src/features/books/booksSlice.ts
+++ global-state-redux/src/features/books/booksSlice.ts
@@ -1,5 +1,5 @@
 import {createSlice} from "@reduxjs/toolkit";
-import {Book} from './books';
+import {Book, InputBook} from './books';
 import booksData from "./booksData";
 import {RootState} from "../../app/store";
 
@@ -9,9 +9,25 @@ export type BooksState = Book[];
 export const booksSlice = createSlice({
     name: 'books',
     initialState: {books: booksData},
-    reducers: {}
+    reducers: {},
 });
 
 export const selectBooks = (state: RootState) => state.books.books
+
+///export const selectRatingFilter = (state: RootState) => state.books.ratingFilter;
+
+export function selectBook(state: RootState): (id?: number) => InputBook {
+    return (id?: number): InputBook => {
+        const book = selectBooks(state).find((book) => book.id === id);
+        if (!book) {
+            return {
+                title: '',
+                author: '',
+                isbn: ''
+            };
+        }
+        return book;
+    };
+}
 
 export default booksSlice.reducer;
