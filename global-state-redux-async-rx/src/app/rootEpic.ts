0da3bdf2f816cefd7622028ffcc195fdafee9cd4
BBK-PiJ-2015-10 <alejandro.palacios@c2fo.com>
2026-02-27 17:55:35 -0500

    reflecting book p2

--- global-state-redux-async-rx/src/app/rootEpic.ts
+++ global-state-redux-async-rx/src/app/rootEpic.ts
@@ -1,6 +1,5 @@
 import {combineEpics} from "redux-observable";
 import booksEpic from '../features/books/books.epics';
 import loginEpic from "../features/login/login.epics";
-import {RootState} from "./store";
 
-export default combineEpics<any, any, RootState>(booksEpic, loginEpic);
+export default combineEpics(booksEpic, loginEpic);
